83f0eeac83d5ab3e47762dcdbd4355f9e2966182
chanakyavamsi <chanakyavamsi11@gmail.com>
2017-08-14 22:24:34 +0530

    First commit

---
+++ app/src/main/java/com/example/gantachanakyavamsi/filter_accelorometer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gantachanakyavamsi.filter_accelorometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
